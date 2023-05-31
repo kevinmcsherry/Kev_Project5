17435a0afdda6d0f993c45ff0a42d5772fdfd8cd
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-05-31 12:42:15 +0000

    Configuring urls and views to align with templates

--- P5_ecommerce/kev_estore/views.py
+++ P5_ecommerce/kev_estore/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def store(request):
+	context = {}
+	return render(request, 'kev_estore/store.html', context)
+
+def cart(request):
+	context = {}
+	return render(request, 'kev_estore/cart.html', context)
+
+def checkout(request):
+	context = {}
+	return render(request, 'kev_estore/checkout.html', context)
