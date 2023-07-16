854164ebce9013c04e71af150d204d88096b0c08
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-07-16 21:22:01 +0000

    Changing database url for production

--- P5_ecommerce/P5_ecommerce/settings.py
+++ P5_ecommerce/P5_ecommerce/settings.py
@@ -84,18 +84,22 @@ WSGI_APPLICATION = 'P5_ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if development:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+
+#if development:
+    #DATABASES = {
+       # 'default': {
+            #'ENGINE': 'django.db.backends.sqlite3',
+            #'NAME': BASE_DIR / 'db.sqlite3',
+        #}
+    #}
+#else:
     # The actual database we want to use for production
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+    #DATABASES = {
+       # 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    #}
 
 
 
