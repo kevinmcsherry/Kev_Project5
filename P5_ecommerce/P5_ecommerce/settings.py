a4315a44af95c24a4ac98f5070018632223b2765
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-07-16 21:17:59 +0000

    Cleaning up commented code and settings file

--- P5_ecommerce/P5_ecommerce/settings.py
+++ P5_ecommerce/P5_ecommerce/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-#SECRET_KEY = 'django-insecure-w66i%a3@_#yy-of3i0u-&fr*x2(*0c)#!4f+5$qdu9z5#0(s@e'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
