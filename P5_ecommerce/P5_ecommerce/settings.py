77904b4e2f3e7b2004ade19480874187dda527e0
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-07-16 21:52:38 +0000

    Edits to settings file

--- P5_ecommerce/P5_ecommerce/settings.py
+++ P5_ecommerce/P5_ecommerce/settings.py
@@ -24,9 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -87,6 +85,10 @@ WSGI_APPLICATION = 'P5_ecommerce.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 #if development:
     #DATABASES = {
