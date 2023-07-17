d358f1578565204ee456fc6b06fedd27abf403e2
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-07-17 17:56:27 +0000

    adding static path

--- P5_ecommerce/settings.py
+++ P5_ecommerce/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
 
-#STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
