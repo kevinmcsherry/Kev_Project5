ddec90c4144993640b92e75dff2c5180305acf50
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-07-17 14:36:45 +0000

    Changes to Settings

--- P5_ecommerce/settings.py
+++ P5_ecommerce/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
 
     'kev_estore.apps.KevEstoreConfig',
 ]
