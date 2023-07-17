9da5875e986a26973fef60dbc4084e5159cc1ac0
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-07-17 12:16:20 +0000

    updating settings file

--- P5_ecommerce/settings.py
+++ P5_ecommerce/settings.py
@@ -145,6 +145,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
 
---
+++ staticfiles/js/basket.js
@@ -0,0 +1,36 @@
+var updateBtns = document.getElementsByClassName('update-basket')
+
+for(var i = 0; i < updateBtns.length; i++){
+	updateBtns[i].addEventListener('click', function(){
+		var golfgearId = this.dataset.golfgear
+		var action = this.dataset.action
+        if(user == 'AnonymousUser'){
+        }else{
+            updateUserOrder(golfgearId, action)
+        }
+    })
+}
+
+
+function updateUserOrder(golfgearId, action){
+    console.log('User is logged in, sending data..')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'golfgearId': golfgearId, 'action': action})
+    })
+
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log('data:', data)
+        location.reload()
+    })
+}
