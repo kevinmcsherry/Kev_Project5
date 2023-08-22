2ff5f95c8972d4c3174261378098db1c47c8a940
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-08-22 12:57:02 +0000

    Updating settings to allow for debug to be variable driven

--- P5_ecommerce/settings.py
+++ P5_ecommerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG')
 # Cross-reference this debug state to set development state
 development = DEBUG
 
