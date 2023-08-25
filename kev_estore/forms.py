0f3cca3f0c00b291db81ca7a2ebc66f014b06d2f
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-08-25 12:49:40 +0000

    Adding newsletter signup form

--- kev_estore/forms.py
+++ kev_estore/forms.py
@@ -1,3 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Customer, User
+from django import forms
+from crispy_forms.helper import FormHelper
+
+
+
+
+
