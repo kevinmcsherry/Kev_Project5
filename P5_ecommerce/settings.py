0359e255484987fd3729158a305667ff90324cfc
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-09-12 17:00:18 +0000

    Changing media root path

--- P5_ecommerce/settings.py
+++ P5_ecommerce/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/media/'
 MEDIAFILES_LOCATION = 'media'
 
